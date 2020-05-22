e9eb60f0f986b2c5738e7be61d95ca02b3cd6f30
matthewmjm <matthewmjm@yahoo.com>
2020-05-21 22:18:51 -0600

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,27 @@
+class Post
+    
+    attr_reader :title
+    attr_accessor :author
+
+    @@all = []
+
+    def initialize title
+        @title = title
+        @@all << self
+    end
+
+
+    def author_name
+        if self.author == nil
+            nil
+        else
+            self.author.name
+        end
+    end
+
+
+    def self.all
+        @@all
+    end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 require_relative "../lib/artist.rb"
 require_relative "../lib/song.rb"
 require_relative "../lib/author.rb"
